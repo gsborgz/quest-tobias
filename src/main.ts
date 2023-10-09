d433e73c136b81c64a22805c5f982b48d6ad8701
gsborgz <borgesgabrielsilva@gmail.com>
2023-10-09 18:59:23 -0300

    chore: opções cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: cors.CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  };
 
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
-  app.enableCors();
+  app.use(cors(corsOptions));
 
   await app.listen(3333);
 
